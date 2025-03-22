13c6e6b71b2da5c3d50d9f647ab7d4b707111083
Daniel Amadi <chimenejnr@gmail.com>
2025-03-22 12:51:43 +0200

    navbar and header done

--- src/pages/home/hero-section.tsx
+++ src/pages/home/hero-section.tsx
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { background_video } from "@/utils/data/constants";
+import { IconButton } from "@chakra-ui/react";
+import { MdVolumeOff, MdVolumeUp } from "react-icons/md";
 
 const HeroSection = () => {
+  const [mute, setMute] = useState<boolean>(true);
   return (
-    <header className="h-auto md:h-[80vh] overflow-hidden flex items-center justify-center">
-      <video src={background_video} autoPlay loop muted />
-      <div className="absolute top-[20vh] md:top-[15vh] right-[20px] w-[15vh] h-[15vh] p-4 bg-black opacity-70 rounded-full flex items-center justify-center uppercase text-white font-bold text-center text-sm md:text-lg font-quicksand hover:rotate-12 hover:bg-[var(--primary)] transition-all duration-500 cursor-pointer">
+    <header className="h-auto md:h-[80vh] overflow-hidden flex items-center justify-center relative">
+      <video src={background_video} autoPlay loop muted={mute} />
+      <div className="absolute top-[10vh] md:top-[5vh] right-[20px] w-[15vh] h-[15vh] p-4 bg-black opacity-70 rounded-full flex items-center justify-center uppercase text-white font-bold text-center text-sm md:text-lg font-quicksand hover:rotate-12 hover:bg-[var(--primary)] transition-all duration-500 cursor-pointer">
         tickets & tables
       </div>
+      <IconButton
+        className="absolute bottom-2 right-[20px] text-white bg-black opacity-70 hover:bg-[var(--primary)] transition-all duration-500"
+        rounded="full"
+        onClick={() => setMute(!mute)}
+      >
+        {mute ? <MdVolumeOff /> : <MdVolumeUp />}
+      </IconButton>
     </header>
   );
 };
