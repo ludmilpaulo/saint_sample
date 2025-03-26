7236eba7137067327c81ea7cf36bd6d0932b6528
Daniel Amadi <chimenejnr@gmail.com>
2025-03-26 19:31:22 +0200

    instagram section added

--- src/api/cloudinary.ts
+++
@@ -1,8 +0,0 @@
-import cloudinary from "cloudinary";
-
-cloudinary.v2.config({
-  cloud_name: "danny-cloud",
-  api_key: "365578963775714",
-  api_secret: "7IrbM6KfehLVynhp714xdqu4epU",
-  secure: true,
-});
